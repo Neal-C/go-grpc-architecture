b56a91a0e7bb52cc35130c7054ce159e878f2c88
Neal-C <pop6d531@duck.com>
2023-11-18 05:47:11 +0100

    finished grpc client for public repo version

---
+++ client/main.go
@@ -0,0 +1,47 @@
+package client
+
+import (
+	"context"
+	"flag"
+	"github.com/Neal-C/go-grpc-architecture/protocodegen"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
+)
+
+func main() {
+	serverAddr := flag.String("server", "localhost:8080", "the server address in format -> host:port")
+
+	flag.Parse()
+
+	// tlsCredentials := credentials.NewTLS(&tls.Config{InsecureSkipVerify: false})
+
+	grpcDialOptions := []grpc.DialOption{
+		// grpc.WithTransportCredentials(tlsCredentials),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFn()
+
+	grpcConnection, err := grpc.DialContext(ctx, *serverAddr, grpcDialOptions...)
+
+	if err != nil {
+		log.Fatalln("failed to dial grpcServer %s", *serverAddr, err)
+	}
+
+	defer grpcConnection.Close()
+
+	grpcClient := protocodegen.NewCalculatorClient(grpcConnection)
+
+	response, err := grpcClient.Sum(ctx, &protocodegen.NumbersRequest{
+		Numbers: []int64{10, 10, 10, 42},
+	})
+
+	if err != nil {
+		log.Fatalln("error sending the &protocodegen.NumbersRequest : ", err)
+	}
+
+	log.Println(response)
+}
